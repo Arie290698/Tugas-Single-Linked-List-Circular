bf544909fe4e0291855893f900d3d30c7a21f4cb
zihadubaddika1004 <38626368+zihadubaddika1004@users.noreply.github.com>
2018-04-30 22:01:54 +0700

    Update fungsi.cpp

--- fungsi.cpp
+++ fungsi.cpp
@@ -28,31 +28,33 @@ void menambah_node_belakang()
 	temp2->next = temp;
 	}
 } 
+
 void menambah_node_depan(){
-linked_list *data_baru;
-data_baru = new linked_list;
-cout << "\tmasukkan nilai data : ";
-cin >> data_baru->urut;
-data_baru->next = NULL;
+	linked_list *data_baru;
+	data_baru = new linked_list;
+	cout << "\tmasukkan nilai data : ";
+	cin >> data_baru->urut;
+	data_baru->next = NULL;
+	
 	
 if(data_ptr == NULL)
 {
-data_ptr=data_baru;
-data_ptr->next = NULL;
+	data_ptr=data_baru;
+	data_ptr->next = NULL;
 }
 else
 {
-data_baru->next = data_ptr;
-data_ptr = data_baru;
+	data_baru->next = data_ptr;
+	data_ptr = data_baru;
 }
 } 
 
 void hapus_node_depan()
 {
-linked_list *temp;
-temp = data_ptr;
-data_ptr = data_ptr->next;
-delete temp;
+	linked_list *temp;
+	temp = data_ptr;
+	data_ptr = data_ptr->next;
+	delete temp;
 } 
 
 
